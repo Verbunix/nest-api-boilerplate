4deac210038d35aa135f918979055ca5614e9780
Andrey Verbitskiy <verb2310@gmail.com>
2020-09-11 21:30:48 +0300

    Fix ormconfig for local development

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,7 +2,7 @@ import {ConnectionOptions} from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'postgres',
+  host: process.env.DB_HOST || '127.0.0.1',
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || 'root',
