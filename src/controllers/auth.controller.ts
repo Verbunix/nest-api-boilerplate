2c653963e3178c34d3fe434ea61abf332ae90cc2
Andrey Verbitskiy <verb2310@gmail.com>
2020-09-17 23:23:27 +0300

    Working on auth

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { SigninDto } from 'src/dtos/signin.dto';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { LoginDto } from '../dtos/login.dto';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Body() body: LoginDto): Promise<any> {
+    return body;
+  }
+
   @Post('/signin')
   async signin(@Body() body: SigninDto): Promise<any> {
     return body;
--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from '../services/auth.service';
+import { LocalStrategy } from '../guards/local.strategy';
+import { UsersService } from '../services/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from '../entities/users.entity';
 import { AuthController } from '../controllers/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [],
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'jwtSecret',
+      signOptions: { expiresIn: '300s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, UsersService],
+  controllers: [AuthController]
 })
-export class AuthModule {
-}
+export class AuthModule {}
